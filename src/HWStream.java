6c08b88c08126efca889e7d5e42d249e26a870db
january89 <eightynine01@naver.com>
2016-06-14 18:40:03 +0900

    Java8 알고리듬 학습!

---
+++ src/HWStream.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.System.out;
+import static java.util.stream.Collectors.toList;
+
+public class HWStream{
+    public static void main(String[] args) {
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork",false,800, Dish.Type.MEAT),
+                new Dish("beef",false,700, Dish.Type.MEAT),
+                new Dish("chicken",false,400, Dish.Type.MEAT),
+                new Dish("french fries",true,530, Dish.Type.OTHER),
+                new Dish("rice",true,350, Dish.Type.OTHER),
+                new Dish("season fruit",true,120, Dish.Type.OTHER),
+                new Dish("pizza",true,550, Dish.Type.OTHER),
+                new Dish("prawns",false,300, Dish.Type.FISH),
+                new Dish("salmon",false,450, Dish.Type.FISH)
+        );
+
+        List<String> threeHighCaloricDishNames;
+        long menuLength = menu.stream().filter(d -> d.getCalories() > 300).count();
+//                        .stream()
+//                        .filter(d -> d.getCalories() > 300).count();
+//        out.println(threeHighCaloricDishNames);
+        out.println(menuLength);
+
+    }
+}
+
+class Dish{
+
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+
+    public Dish(String name, boolean vegetarian, int calories, Type type) {
+        this.name = name;
+        this.vegetarian = vegetarian;
+        this.calories = calories;
+        this.type = type;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public boolean isVegetarian(){
+        return vegetarian;
+    }
+    public int getCalories(){
+        return calories;
+    }
+    public Type getType(){
+        return type;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+
+    public enum Type{
+        MEAT,FISH,OTHER
+    }
+
+}
